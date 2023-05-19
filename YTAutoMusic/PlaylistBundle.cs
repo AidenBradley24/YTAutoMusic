34bcbb5165451bfbc9d13c2c36dba0f4b96bc6cb
Aiden Bradley <AwesomeAiden24@outlook.com>
2023-05-18 20:13:23 -0400

    Selection of options

---
+++ YTAutoMusic/PlaylistBundle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTAutoMusic
+{
+    internal class PlaylistBundle
+    {
+        public PlaylistBundle()
+        {
+            Console.WriteLine("Create new folder? y/n");
+        }
+    }
+}
--- YTAutoMusic/Program.cs
+++ YTAutoMusic/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("YTAutoMusic\n----------\n");
+            
+            if(args.Length == 0 )
+            {
+                NoArgument();
+            }
+
+        }
+
+        static void NoArgument()
+        {
+            string response;
+
+            do
+            {
+                Console.WriteLine("What do you want to do?\n'n' - new playlist | 'a' - append playlist | 'q' - quit");
+                response = Console.ReadLine();
+
+                switch (response)
+                {
+                    case "q":
+                        Environment.Exit(0);
+                        break;
+                    case "n":
+                        var playlist = new PlaylistBundle();
+                        break;
+                    case "a":
+                        break;
+                }
+
+            } while (true);
         }
     }
 }
