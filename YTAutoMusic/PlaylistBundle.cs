4d17109fba6b6ae3bf50e5598e50a30426a482a5
Aiden Bradley <AwesomeAiden24@outlook.com>
2023-05-19 22:39:02 -0400

    basic functionality complete

--- YTAutoMusic/PlaylistBundle.cs
+++ YTAutoMusic/PlaylistBundle.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TagLib;
 
 namespace YTAutoMusic
 {
@@ -24,132 +20,136 @@ namespace YTAutoMusic
                 folder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
             }
 
-            Console.WriteLine("Give YT playlist URL.");
-            string url = Console.ReadLine();
-
-            if(string.IsNullOrWhiteSpace(url))
-            {
-                throw new FormatException("Must be a playlist url");
-            }
-
             var tempDirectory = Directory.CreateDirectory(folder + @"\temp");
             var baseDirectory = tempDirectory.Parent;
 
+            string url;
+            int listIndex;
 
-            // get playlist ID
-            int listIndex = url.IndexOf(LIST_URL) + LIST_URL.Length;
-
-            if(listIndex == -1)
+            while (true)
             {
-                throw new FormatException("Must be a playlist url");
+                Console.WriteLine("\nProvide a YouTube playlist URL:");
+                url = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(url))
+                {
+                    Console.WriteLine("\nInvalid URL\n");
+                    continue;
+                }
+
+                listIndex = url.IndexOf(LIST_URL) + LIST_URL.Length;
+
+                if (listIndex == -1)
+                {
+                    Console.WriteLine("\nInvalid URL\n");
+                    continue;
+                }
+
+                break;
             }
 
             string playlistID = url[listIndex..];
 
-            //
+            // use yt-dlp to download from youtube
 
-            try
+            var ytDLP = new Process();
+            ytDLP.StartInfo.FileName = Environment.ExpandEnvironmentVariables(@"%PROGRAMFILES%\yt-dlp\yt-dlp.exe");
+            ytDLP.StartInfo.Arguments = $"\"{url}\" -P \"{tempDirectory}\" -f bestaudio --no-overwrites --yes-playlist --no-write-comments --write-description --write-playlist-metafiles";
+            ytDLP.Start();
+            ytDLP.WaitForExit();
+            ytDLP.Dispose();
+
+            var files = tempDirectory.EnumerateFiles();
+            var descriptionFiles = files.Where(f => f.Name.EndsWith(".description"));
+            var soundFiles = files.Where(f => !f.Name.EndsWith(".description"));
+            var matches = descriptionFiles.Where(f => f.Name.Contains($"[{playlistID}]"));
+
+            string playlistName;
+
+            if (!matches.Any())
             {
-                var ytDLP = new Process();
-                ytDLP.StartInfo.FileName = Environment.ExpandEnvironmentVariables(@"%PROGRAMFILES%\yt-dlp\yt-dlp.exe");
-                ytDLP.StartInfo.Arguments = $"\"{url}\" -P \"{tempDirectory}\" -f bestaudio --no-overwrites --yes-playlist --no-write-comments --write-description --write-playlist-metafiles";
-                ytDLP.Start();
+                Console.WriteLine("No name found giving default name.");
+                playlistName = $"Playlist [{playlistID}]";
+            }
+            else
+            {
+                var file = matches.First();
 
-                ytDLP.WaitForExit();
+                playlistName = GetNameWithoutURLTag(file.Name);
+                var invalids = Path.GetInvalidFileNameChars();
+                playlistName = string.Join("_", playlistName.Split(invalids, StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.');
+            }
 
-                ytDLP.Dispose();
+            Console.WriteLine($"Creating Playlist \"{playlistName}\"");
+            var finalDirectory = Directory.CreateDirectory(baseDirectory + @$"\{playlistName}");
 
-                Console.WriteLine(tempDirectory);
+            var ffmpeg = new Process();
+            ffmpeg.StartInfo.FileName = Environment.ExpandEnvironmentVariables(@"%PROGRAMFILES%\ffmpeg\ffmpeg.exe");
 
-                var files = tempDirectory.EnumerateFiles();
-                var descriptionFiles = files.Where(f => f.Name.EndsWith(".description"));
-                var soundFiles = files.Where(f => !f.Name.EndsWith(".description"));
+            List<(FileInfo, string, string)> newSound = new(soundFiles.Count());
+            foreach (var sound in soundFiles)
+            {
+                string originalName = sound.FullName;
+                string rawName = GetNameWithoutURLTag(sound.Name);
+                string newName = finalDirectory + @$"\{rawName}.mp3";
 
-                var matches = descriptionFiles.Where(f => f.Name.Contains($"[{playlistID}]"));
-                
-                string playlistName;
-                if(!matches.Any())
+                Console.WriteLine($"\n\nConverting to mp3: '{originalName}'\n" +
+                    $"\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n" +
+                    $"'{newName}'\n");
+
+                ffmpeg.StartInfo.Arguments = $"-i \"{originalName}\" \"{newName}\"";
+                ffmpeg.Start();
+                ffmpeg.WaitForExit();
+
+                sound.Delete(); // delete original file
+
+                FileInfo newFile = new(newName);
+                newSound.Add((newFile, GetURLTag(sound.Name), rawName));
+            }
+
+            foreach (var bundle in newSound)
+            {
+                (FileInfo sound, string id, string name) = bundle;
+
+                Console.WriteLine();
+                Console.WriteLine($"\n{sound.FullName}");
+
+                Console.WriteLine($"Formatting '{name}' ; ID: '{id}'");
+
+                var file = TagLib.File.Create(sound.FullName);
+                file.Tag.DateTagged = DateTime.Now;
+
+                // youtube description
+
+                matches = descriptionFiles.Where(f => f.Name.Contains($"[{id}]"));
+
+                string description = $"Created from a YouTube video." +
+                $"URL: youtube.com/watch?v={id}\n";
+
+                if (!matches.Any())
                 {
-                    Console.WriteLine("No name found giving default name.");
-                    playlistName = "Playlist #" + playlistID;
+                    Console.WriteLine($"No description found for \"{name}\" giving defaults.");
                 }
                 else
                 {
-                    var file = matches.First();
-
-                    playlistName = GetNameWithoutURLTag(file.Name);
-                    var invalids = Path.GetInvalidFileNameChars();
-                    playlistName = string.Join("_", playlistName.Split(invalids, StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.');
-                }
-
-                Console.WriteLine($"Creating Playlist \"{playlistName}\"");
-                var finalDirectory = Directory.CreateDirectory(baseDirectory + @$"\{playlistName}");
-
-                var ffmpeg = new Process();
-                ffmpeg.StartInfo.FileName = Environment.ExpandEnvironmentVariables(@"%PROGRAMFILES%\ffmpeg\ffmpeg.exe");         
-
-                List<FileInfo> newSound = new List<FileInfo>(soundFiles.Count());
-                foreach(var sound in soundFiles)
-                {
-                    ffmpeg.StartInfo.Arguments = $"-i {sound.FullName} {finalDirectory + @$"\{sound.Name[..sound.Name.LastIndexOf('.')]}.mp3"}";
-                    ffmpeg.Start();
-                    ffmpeg.WaitForExit();
-                }
-
-                foreach (var sound in newSound)
-                {
-                    Console.WriteLine(sound.FullName);
-
-                    var rawName = sound.Name[..sound.Name.LastIndexOf('.')];
-
-                    matches = descriptionFiles.Where(f => f.Name[..f.Name.LastIndexOf('.')] == rawName);
-                    var id = GetURLTag(rawName);
-
-                    Console.WriteLine($"Formatting '{rawName}' ; ID: '{id}'");
-
-                    if (!matches.Any())
+                    using (var stream = matches.First().OpenText())
                     {
-                        Console.WriteLine($"No name found for \"{rawName}\" giving defaults.");
-                        playlistName = $"Playlist [{playlistID}]";
-
-                        var file = TagLib.File.Create(sound.FullName);
-                        file.Tag.DateTagged = DateTime.Now;
-
-                        string description = $"Created from a YouTube video." +
-                            $"URL: youtube.com/watch?v={id}\n";
-
-                        using (var stream = descriptionFiles.Where(f => f.Name.Contains($"[{id}]")).First().OpenText())
+                        string addition = stream.ReadToEnd();
+                        if (!string.IsNullOrEmpty(addition))
                         {
-                            string addition = stream.ReadToEnd();
-                            if (!string.IsNullOrEmpty(addition))
-                            {
-                                description += $"\n--- ORIGINAL DESCRIPTION ---\n" + addition;
-                            }
+                            description += $"\n--- ORIGINAL DESCRIPTION ---\n" + addition;
                         }
-
-                        file.Tag.Description = description;
-                        file.Tag.Album = playlistName;
-                        file.Tag.Title = GetNameWithoutURLTag(sound.Name);
-                    }
-                    else
-                    {
-                        var file = matches.First();
-                        string tempName = file.Name;
-                        playlistName = tempName[..tempName.LastIndexOf('[')];
-                        var invalids = Path.GetInvalidFileNameChars();
-                        playlistName = string.Join("_", playlistName.Split(invalids, StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.');
                     }
                 }
 
-                tempDirectory.Delete(true);
+                file.Tag.Description = description;
+                file.Tag.Album = playlistName;
+                file.Tag.Title = name;
+
+                file.Save();
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error has occured!");
-                Console.WriteLine(ex.ToString());
-                Environment.Exit(1);
-                tempDirectory.Delete(true);
-            }
+
+            tempDirectory.Delete(true);
         }
 
         private static string GetNameWithoutURLTag(string name)
@@ -159,7 +159,7 @@ namespace YTAutoMusic
 
         private static string GetURLTag(string name)
         {
-            return name[(name.LastIndexOf('[')+1)..name.LastIndexOf(']')];
+            return name[(name.LastIndexOf('[') + 1)..name.LastIndexOf(']')];
         }
     }
 }
