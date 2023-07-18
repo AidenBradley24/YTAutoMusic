128acac5fab075e7d90fede76604c304ac7624ab
Aiden Bradley <AwesomeAiden24@outlook.com>
2023-07-17 22:28:35 -0400

    Adding some soundtrack tests. Some of them fail so work needs to be done on soundtrack filler

--- YTAutoMusic/MetadataFillerExtensions.cs
+++ YTAutoMusic/MetadataFillerExtensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace YTAutoMusic
 {
-    internal static class MetadataFillerExtensions
+    public static class MetadataFillerExtensions
     {
         public static readonly char[] SEPERATORS = { '-', '\u2012', '\u2013', '\u2014', '\u2015', '|', '·', '\uFF02', '\u0022', '\u201C', '\u201D', '\u201E', '\u201F' };
 
